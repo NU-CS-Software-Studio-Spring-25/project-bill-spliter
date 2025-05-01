d1b8c97a8ca2fc2c96e8fca53c291dc24a0963ed
mordonez719 <mordonez1018@gmail.com>
2025-04-30 21:57:22 -0500

    routes to home

--- bill_splitter/config/routes.rb
+++ bill_splitter/config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "expenses#index"
+
   namespace :api do
     namespace :v1 do
       resources :expenses, only: [:index,:create,:destroy]

3ab58d3dfb98196eda6ca374069c32e223afc099
davidyim03 <davidyim03@gmail.com>
2025-05-13 00:41:44 -0500

    deployment ready

--- server/config/application.rb
+++ server/config/application.rb
@@ -23,6 +23,6 @@ module BillSplitter
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.api_only = true
   end
 end

d9c6833047e854579f4a860cc323961ca135f7fd
davidyim03 <davidyim03@gmail.com>
2025-05-30 20:34:35 -0500

    cors fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173' # Add your frontend URLs
+    origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173', 'https://bill-splitter-api-d46b8052a10f.herokuapp.com' # Add your frontend URLs
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
